426cca62a91fa101ab4e5ffe4ef0d9fbe022752b
Will Schiller <will.schiller@protonmail.com>
2023-09-23 16:33:55 -0400

    restyle and start implimenting CCIP

--- src/Plugin.sol
+++ src/Plugin.sol
@@ -1,26 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-
+//CCIP
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {LinkTokenInterface} from "@chainlink/contracts-ccip/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
-
+//SAFE
 import {BasePluginWithEventMetadata, PluginMetadata} from "./Base.sol";
 import {ISafe} from "@safe/interfaces/Accounts.sol";
 import {ISafeProtocolManager} from "@safe/interfaces/Manager.sol";
 import {SafeTransaction, SafeProtocolAction} from "@safe/DataTypes.sol";
 import {IPool} from "@aave/interfaces/IPool.sol";
 
-contract Plugin is BasePluginWithEventMetadata {
-    constructor(address poolAddress)
-        BasePluginWithEventMetadata(
-            PluginMetadata({name: "Fillable.xyz", version: "1.0.0", requiresRootAccess: false, iconUrl: "", appUrl: ""})
-        )
-    {
-        pool = poolAddress;
-    }
+contract Plugin is BasePluginWithEventMetadata, OwnerIsCreator {
 
     enum Network {
         ARBITRUM_GOERLI,
@@ -29,15 +22,34 @@ contract Plugin is BasePluginWithEventMetadata {
         POLYGON_MUMBAI
     }
 
-    error ThreadLocked();
-    error RecipientNotWhitelisted();
-    error AaveHealthFactorTooHigh();
-
     bool private isPool; // true is pool false is child
     mapping(Network network => mapping(address to => mapping(address from => bool locked))) threadLocked; // Prevent duplication
     mapping(address from => mapping(Network network => mapping(address to => bool isWhitelisted))) private
         whitelistedRecipient; //should be gas optimised
     address private pool; // pool address
+
+    //CCIP
+    IRouterClient router;
+    LinkTokenInterface linkToken;
+
+    event MessageSent( // The unique ID of the CCIP message.
+        // The chain selector of the destination chain.
+        // The address of the receiver on the destination chain.
+        // The text being sent.
+        // the token address used to pay CCIP fees.
+        // The fees paid for sending the CCIP message.
+        bytes32 indexed messageId,
+        uint64 indexed destinationChainSelector,
+        address receiver,
+        string text,
+        address feeToken,
+        uint256 fees
+    );
+
+    error ThreadLocked();
+    error RecipientNotWhitelisted();
+    error AaveHealthFactorTooHigh();
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); // Used to make sure contract has enough balance.
 
     modifier guard(address _to, address _from, Network network) {
         if (threadLocked[network][_to][_from]) revert ThreadLocked();
@@ -48,6 +60,22 @@ contract Plugin is BasePluginWithEventMetadata {
             revert AaveHealthFactorTooHigh();
         }
         _;
+    }
+
+    constructor(address _pool, address _router, address _link)
+        BasePluginWithEventMetadata(
+            PluginMetadata({
+                name: "Fillable.xyz",
+                version: "1.0.0",
+                requiresRootAccess: false,
+                iconUrl: "Fillable.xyz",
+                appUrl: ""
+            })
+        )
+    {
+        pool = _pool;
+        router = IRouterClient(_router);
+        linkToken = LinkTokenInterface(_link);
     }
 
     function getAaveUserHealth(address userAddress) public view returns (uint256) {
